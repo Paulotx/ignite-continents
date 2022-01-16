635020577d5de84c302f733e71c0fcdc9bc4d100
Paulotx <pauloandre0011@gmail.com>
2022-01-16 01:03:43 -0300

    add chakra, fonts and config theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme, Theme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      '500': '#47585B',
+    },
+    yellow: {
+      '500': '#FFBA08',
+    },
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white',
+        color: 'gray.500',
+      },
+    },
+  },
+} as Theme | {});
